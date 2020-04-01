0d7f42aa2e76969877a875d75c51e966b4d61155
didi <git@d10r.net>
2020-04-01 12:35:18 +0200

    always show footer, removed unused about page

--- app/src/app.js
+++ app/src/app.js
@@ -7,7 +7,6 @@ export class App {
       {route: 'authenticate', name: 'authenticate', moduleId: 'authenticate', nav: true, title: 'Authentifizieren'},
       {route: 'vote', name: 'vote', moduleId: 'vote', nav: true, title: 'Abstimmen'},
       {route: 'result', name: 'result', moduleId: 'result', nav: true, title: 'Ergebnis'},
-      {route: 'about', name: 'about', moduleId: 'about', nav: false},
       {route: 'test', name: 'test', moduleId: 'test', nav: false}
     ]);
 
